7da8a19feb4d134ffa21a1c2a304d60ad0c5a982
randall.erasmus <randallerasmus1@gmail.com>
2022-10-19 23:24:46 +0200

    Getting the application properties up to date and adding a User Class

---
+++ src/main/java/za/co/randall/jobapplicationbackend/JobapplicationbackendApplication.java
@@ -0,0 +1,13 @@
+package za.co.randall.jobapplicationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapplicationbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobapplicationbackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/za/co/randall/jobapplicationbackend/models/User.java
@@ -0,0 +1,7 @@
+package za.co.randall.jobapplicationbackend.models;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+public class User {
+}
